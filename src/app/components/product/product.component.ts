a2ec5d7fddabd04ee5229c60a6cdf297588d3f0a
RReDDpy <grandeandre23@gmail.com>
2025-04-09 20:18:25 +0100

    Final error fixing

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Funko} from '../../../funko';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
