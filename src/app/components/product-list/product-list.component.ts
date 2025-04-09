eb7e2632d416ce26be429d73c9efac83ad78065b
RReDDpy <grandeandre23@gmail.com>
2025-04-09 20:17:21 +0100

    Fixed error where products didnt show up on main page and changed package.json so when executing a window pops up

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import {Funko} from '../../../funko';
 import {ProductComponent} from '../product/product.component';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FilterService } from '../../services/filter.service';
+import {FiguresService} from '../../../figures.service';
 
-
-interface ParsedFunko extends Omit<Funko, 'price'> {
-  price: number;
-}
 
 @Component({
   selector: 'app-product-list',
@@ -18,68 +14,22 @@ interface ParsedFunko extends Omit<Funko, 'price'> {
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnChanges {
-  funkos: ParsedFunko[] = [];
-  filteredFunkos: ParsedFunko[] = [];
+export class ProductListComponent implements OnInit {
+  funkos: Funko[] = [];
   loading = true;
   errorMessage = '';
 
-  // Valores predeterminados para los filtros
-  priceFilter: number = 100;
-  selectedCategories: Set<string> = new Set();
+  funkoService = inject(FiguresService);
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private activatedRoute: ActivatedRoute, // Para leer los parámetros de la URL
-    private filterService: FilterService
-  ) {}
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.funkos = this.funkoService.getAllFunkos();
+  }
 
   ngOnInit(): void {
-    this.loadFunkos();
-
-    // Suscripción para escuchar los cambios en los parámetros de la URL
-    this.activatedRoute.queryParams.subscribe(params => {
-      if (params['price']) {
-        this.priceFilter = Number(params['price']);
-      }
-      if (params['categories']) {
-        this.selectedCategories = new Set(params['categories'].split(','));
-      }
-
-      this.applyFilters({
-        maxPrice: this.priceFilter,
-        categories: Array.from(this.selectedCategories)
-      });
-    });
-
-    this.filterService.filters$.subscribe((filters) => {
-      this.applyFilters(filters);
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['priceFilter'] || changes['selectedCategories']) {
-      this.applyFilters({
-        maxPrice: this.priceFilter,
-        categories: Array.from(this.selectedCategories)
-      });
-    }
-  }
-
-  loadFunkos(): void {
     this.http.get<Funko[]>('assets/funkos_data.json').subscribe(
       (data: Funko[]) => {
-        this.funkos = data.map(funko => ({
-          ...funko,
-          price: parseFloat(funko.price.replace(',', '.')) // Convierte de string a number
-        }));
-
-        this.applyFilters({
-          maxPrice: this.priceFilter,
-          categories: Array.from(this.selectedCategories)
-        });
-
+        this.funkos = data;
         this.loading = false;
       },
       (error) => {
@@ -89,16 +39,7 @@ export class ProductListComponent implements OnInit, OnChanges {
     );
   }
 
-
-  applyFilters(filters: { maxPrice: number; categories: string[] }): void {
-    this.filteredFunkos = this.funkos.filter(funko =>
-      funko.price <= filters.maxPrice &&
-      (filters.categories.length === 0 || filters.categories.includes(funko.series))
-    );
-  }
-
   viewProductDetailled(productId: number) {
     this.router.navigate(['/product-detail', productId]);
   }
 }
-
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {Funko} from '../../../funko';
-import {ProductDetailledComponent} from '../product-detailled/product-detailled.component';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
-  imports: [ProductDetailledComponent],
+  imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: '../product-list/product-list.component.css'
 })
 export class ProductComponent {
   @Input() funko!: Funko;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  viewProductDetailled(productId: number) {
+    this.router.navigate(['/product-detail', productId]);
+  }
 }
