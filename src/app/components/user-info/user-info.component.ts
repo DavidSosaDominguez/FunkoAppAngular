8d3c7be818dc840efcb2ac2264aac633fab5453a
DaniSosa <dnsosdom@gmail.com>
2025-05-19 18:42:09 +0100

    Created user info page

---
+++ src/app/components/user-info/user-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {DataBaseUser} from '../../interfaces/user';
+
+@Component({
+  selector: 'app-user-info',
+  imports: [],
+  templateUrl: './user-info.component.html',
+  styleUrl: './user-info.component.css',
+  standalone: true
+})
+export class UserInfoComponent {
+}
